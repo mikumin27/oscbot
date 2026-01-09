b0e23a63b2e27166c3e1b931f2df58f5f741b76f
ibuergis <ian@buergis.com>
2026-01-09 06:25:42 +0100

    Protect from bad youtube api

--- src/events/background_tasks/refresh_feed.rs
+++ src/events/background_tasks/refresh_feed.rs
@@ -5,25 +5,25 @@ use std::{env, sync::{Mutex, OnceLock}};
 
 use crate::{Error, defaults::NEW_VIDEOS_CHANNEL};
 
-static LAST_VIDEO_ID: OnceLock<Mutex<Option<String>>> = OnceLock::new();
+static LAST_VIDEO_ID: OnceLock<Mutex<Option<Vec<String>>>> = OnceLock::new();
 
-fn value() -> &'static Mutex<Option<String>> {
+fn value() -> &'static Mutex<Option<Vec<String>>> {
     LAST_VIDEO_ID.get_or_init(|| Mutex::new(None))
 }
 
-fn set(s: String) {
+fn set(s: Vec<String>) {
     *value().lock().unwrap() = Some(s);
 }
 
-fn get_clone() -> Option<String> {
+fn get_clone() -> Option<Vec<String>> {
     value().lock().unwrap().clone()
 }
 
 pub async fn refresh_feed(ctx: &serenity::Context) -> Result<(), Error> {
     tracing::debug!("checking youtube for new video uploads");
     let url = format!("https://www.youtube.com/feeds/videos.xml?channel_id={}", env::var("OSC_BOT_YOUTUBE_CHANNEL_ID").unwrap());
-    let last_video_id = get_clone();
-    tracing::debug!(last = last_video_id);
+    let last_video_ids = get_clone();
+    tracing::debug!(last = last_video_ids.clone().unwrap_or(vec!["empty".to_string()]).join(", "));
 
     let c = reqwest::Client::new();
     let req = c.get(&url);
@@ -36,23 +36,22 @@ pub async fn refresh_feed(ctx: &serenity::Context) -> Result<(), Error> {
 
     tracing::debug!(video_ids = video_ids.join(", "));
 
-    if video_ids.len() > 0 {
-        set(video_ids[0].clone());
-    }
 
-    let unwrapped_video_id = match last_video_id {
-        Some(video_id) => video_id,
+    let unwrapped_video_ids = match last_video_ids {
+        Some(video_ids) => video_ids,
         None => {
+            set(video_ids);
             tracing::debug!("first loop... checking for video uploads is skipped");
             return Ok(())
         }
     };
 
-    for video_id in video_ids {
-        if video_id == unwrapped_video_id {
+    for video_id in &video_ids {
+        if unwrapped_video_ids.contains(video_id) {
             tracing::debug!("checking for new uploads has finished!");
             return Ok(())
         }
+        set(video_ids.clone());
         tracing::info!(link = format!("https://youtu.be/{}", video_id), "New upload has been found!");
         NEW_VIDEOS_CHANNEL.send_message(ctx,
             CreateMessage::default().content(format!("A new score has been uploaded!\nhttps://youtu.be/{}", video_id))
